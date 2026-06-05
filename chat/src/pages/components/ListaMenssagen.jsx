0228829a75562f3abcc06f4c3ad2862b9ac4c683
unknown <Eric6ix>
2026-06-05 14:22:32 -0300

    componentes e arquivos de configuração não utilizados. em jsx

--- chat/src/pages/components/ListaMenssagen.jsx
+++
@@ -1,9 +0,0 @@
-export const ListaMenssagen = ({ pergunta }) => {
-    return(
-        <div>
-            {pergunta.map((mensagem) => (
-                <div>{mensagem.id}</div>
-            ))}
-        </div>
-    )
-}
