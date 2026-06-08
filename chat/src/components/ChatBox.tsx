b7b5255682e1f68d3cd9b5480f7c4960b6c4e122
unknown <Eric6ix>
2026-06-08 12:26:33 -0300

    - Fix: envio de perguntas duplicadas pel usuário. - Fix: Avisos - Fix: Css não renderizada com condição 'usuario'

--- chat/src/components/ChatBox.tsx
+++ chat/src/components/ChatBox.tsx
@@ -6,20 +6,22 @@ interface ChatBoxProps {
   desabilitado?: boolean;
 }
 
-export const ChatBox = ({ onEnviarMensagem, desabilitado = false }: ChatBoxProps): JSX.Element => {
-  const [inputValue, setInputValue] = useState("");
+export const ChatBox = ({onEnviarMensagem, desabilitado = false,}: ChatBoxProps): JSX.Element => {
+const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const texto = inputValue.trim();
+    if (!texto) return;
+    onEnviarMensagem(texto);
+    setInputValue("");
+  };
+
 
   return (
-
     <div className="border border-gray-300/80 p-4">
       <form
         className="flex space-x-3 border-t border-gray-300 rounded-full"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (!inputValue.trim()) return;
-          onEnviarMensagem(inputValue.trim());
-          setInputValue("");
-        }}
+        onSubmit={handleSubmit}
       >
         <input
           value={inputValue}
@@ -28,13 +30,13 @@ export const ChatBox = ({ onEnviarMensagem, desabilitado = false }: ChatBoxProps
           placeholder="Digite sua pergunta sobre receitas..."
           disabled={desabilitado}
           className="flex-1 px-5 py-3 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-200 outline-none focus:ring-offset-2
-           focus:ring-offset-gray-400 transition-colors disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-500"
+           focus:ring-offset-gray-400 transition-colors disabled:bg-gray-200 disabled:text-gray-500"
         />
         <button
           type="submit"
           disabled={desabilitado || !inputValue.trim()}
-          className="px-8 py-3 bg-gradient-to-r from-purple-300 to-emerald-400 text-white rounded-full hover:from-purple-400 hover:to-emerald-500
-           transition-colors disabled:opacity-50 disabled:cursor-not-allowed cursor-not-allowed hover:to-purple-300 hover:to-emerald-400"
+          className="px-8 py-3 bg-linear-to-r from-purple-300 to-emerald-400 text-white rounded-full hover:from-purple-400 hover:to-emerald-500
+           transition-colors disabled:opacity-50 disabled:cursor-not-allowed enabled:cursor-pointer"
         >
           Enviar
         </button>
@@ -42,4 +44,3 @@ export const ChatBox = ({ onEnviarMensagem, desabilitado = false }: ChatBoxProps
     </div>
   );
 };
-
