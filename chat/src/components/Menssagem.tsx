2fc8519c25dcd62bcd3a0d2012de36ee9ea3af2c
unknown <Eric6ix>
2026-06-08 12:13:49 -0300

    Atualizei o arquivo .gitignore para incluir o diretório 'dist', modifiquei os estilos do componente Menssagem e removi o StrictMode do arquivo main.tsx.

--- chat/src/components/Menssagem.tsx
+++ chat/src/components/Menssagem.tsx
@@ -16,7 +16,7 @@ export const Menssagem = ({ pergunta }: { pergunta: Mensagem }): JSX.Element => 
           "max-w-xs lg:max-w-md px-4 py-3 shadow-lg cursor-pointer select-text transition-transform active:scale-[0.99] hover:shadow-xl " +
           (isIA
             ? "rounded-tl-2xl rounded-tr-md rounded-br-2xl rounded-bl-md bg-gray-50 text-gray-900 border border-gray-300 "
-            : "rounded-tl-md rounded-tr-2xl rounded-br-md rounded-bl-2xl bg-gradient-to-r from-purple-300 to-emerald-400 text-white border border-white/30")
+            : "rounded-tl-md rounded-tr-2xl rounded-br-md rounded-bl-2xl bg-linear-to-r from-purple-300 to-emerald-400 text-white border border-white/30")
         }
       >
         <p className="text-sm leading-relaxed whitespace-pre-line">
